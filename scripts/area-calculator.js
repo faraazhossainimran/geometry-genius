b5719db73ae7a8be9b91ebdbe31717bce9826629
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-08-19 03:30:28 +0600

    some js added

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -5,16 +5,41 @@ function calculateTriangleArea(){
     // const triangleIdBaseNumber = parseFloat(triangleIdBaseString);
     const triangleIdBaseNumber = getInputValue('triangle-base')
     // height
-    const triangleHeightId = document.getElementById('triangle-height');
-    const triangleIdHeightString = triangleHeightId.value;
-    const triangleIdHeightNumber = parseFloat(triangleIdHeightString);
+    // const triangleHeightId = document.getElementById('triangle-height');
+    // const triangleIdHeightString = triangleHeightId.value;
+    // const triangleIdHeightNumber = parseFloat(triangleIdHeightString);
+    const triangleIdHeightNumber = getInputValue('triangle-height')
     const area = 0.5 * triangleIdBaseNumber * triangleIdHeightNumber;
-    console.log(area)
+    // console.log(area)
 
     // area 
     const triangleAreaId = document.getElementById('triangle-area');
-    console.log(triangleAreaId.innerText)
     triangleAreaId.innerText = area;
+}
+
+// Calculate Rectangle
+function calculateRectangleArea(){
+    const rectangleWidth = getInputValue('rectangle-width');
+    const rectangleHeight = getInputValue('rectangle-length')
+    const rectangle = rectangleWidth * rectangleHeight;
+    const rectangleAreaId = document.getElementById('rectangle-area')
+    rectangleAreaId.innerText = rectangle;
+}
+
+
+function calculateParallelogramArea(){
+    const ParallelogramBase = getInputValue('parallelogram-base');
+    const ParallelogramHeight = getInputValue('parallelogram-height')
+    const Parallelogram = ParallelogramBase * ParallelogramHeight;
+    const ParallelogramAreaId = document.getElementById('parallelogram-area')
+    ParallelogramAreaId.innerText = Parallelogram;
+}
+
+
+// add to calculation entry 
+function addToCalculationEntry (){
+    const calculationEntryId = getInputValue('calculation-entry');
+    console.log(calculationEntryId);
 }
 
 
@@ -26,4 +51,4 @@ function getInputValue(inputId){
     return inputValueNumber;
 }
 
-// console.log(getInputValue(5))
+// useable function for area innerText
