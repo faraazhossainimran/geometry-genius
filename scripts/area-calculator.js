ff519ebeaa0131fcd68106f8d519e355135e5e05
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-08-20 00:34:03 +0600

    reusable function added

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -3,18 +3,21 @@ function calculateTriangleArea(){
     // const triangleBaseId = document.getElementById('triangle-base');
     // const triangleIdBaseString = triangleBaseId.value;
     // const triangleIdBaseNumber = parseFloat(triangleIdBaseString);
-    const triangleIdBaseNumber = getInputValue('triangle-base')
     // height
     // const triangleHeightId = document.getElementById('triangle-height');
     // const triangleIdHeightString = triangleHeightId.value;
     // const triangleIdHeightNumber = parseFloat(triangleIdHeightString);
+
+    const triangleIdBaseNumber = getInputValue('triangle-base')
     const triangleIdHeightNumber = getInputValue('triangle-height')
     const area = 0.5 * triangleIdBaseNumber * triangleIdHeightNumber;
-    // console.log(area)
 
-    // area 
-    const triangleAreaId = document.getElementById('triangle-area');
-    triangleAreaId.innerText = area;
+    // show Triangle areaarea old way
+    // const triangleAreaId = document.getElementById('triangle-area');
+    // triangleAreaId.innerText = area;
+
+    // show Triangle area using resueable funciton
+    setElementInnerText('triangle-area', area)
 }
 
 // Calculate Rectangle
@@ -22,19 +25,27 @@ function calculateRectangleArea(){
     const rectangleWidth = getInputValue('rectangle-width');
     const rectangleHeight = getInputValue('rectangle-length')
     const rectangle = rectangleWidth * rectangleHeight;
-    const rectangleAreaId = document.getElementById('rectangle-area')
-    rectangleAreaId.innerText = rectangle;
+    // show rectangle area old way
+    // const rectangleAreaId = document.getElementById('rectangle-area')
+    // rectangleAreaId.innerText = rectangle;
+
+    // show rectangle area using resueable funciton
+    setElementInnerText('rectangle-area', rectangle)
 }
 
-
+// Calculate Parallelogram
 function calculateParallelogramArea(){
     const ParallelogramBase = getInputValue('parallelogram-base');
     const ParallelogramHeight = getInputValue('parallelogram-height')
     const Parallelogram = ParallelogramBase * ParallelogramHeight;
-    const ParallelogramAreaId = document.getElementById('parallelogram-area')
-    ParallelogramAreaId.innerText = Parallelogram;
-}
 
+    // show Pralleloram area old way
+    // const ParallelogramAreaId = document.getElementById('parallelogram-area')
+    // ParallelogramAreaId.innerText = Parallelogram;
+
+    // show Pralleloram area new way
+    setElementInnerText('parallelogram-area', Parallelogram)
+}
 
 // add to calculation entry 
 function addToCalculationEntry (){
@@ -42,8 +53,7 @@ function addToCalculationEntry (){
     console.log(calculationEntryId);
 }
 
-
-// useable function for input 
+// reuseable function for input 
 function getInputValue(inputId){
     const inputValue = document.getElementById(inputId);
     const inputValueString = inputValue.value;
@@ -51,4 +61,10 @@ function getInputValue(inputId){
     return inputValueNumber;
 }
 
-// useable function for area innerText
+// reuseable set span, p, div, ect text
+function setElementInnerText (elementId, area){
+    const element = document.getElementById(elementId)
+    element.innerText = area;
+}
+
+// data validation
